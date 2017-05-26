b5dd14b1aa7270fc26162a604f95a5df2da711e4
Pete Savage <psavage@redhat.com>
2017-05-26 12:35:25 +0100

    Updated

--- jvis/urls.py
+++ jvis/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^jvis/', views.gui),
-    url(r'^detail/', views.detail)
+    url(r'^jvis/', views.gui, name='jvis'),
+    url(r'^detail/', views.detail, name='detail')
 ]
